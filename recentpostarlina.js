9ca7ca9425ff2b2efb6022b465bec9b36e655298
William Rinaldo <51898911+williamrinaldo@users.noreply.github.com>
2019-07-19 11:28:27 +0700

    Add files via upload

--- recentpostarlina.js
+++ recentpostarlina.js
@@ -1,110 +1,15 @@
-function recentpostinfoarlina(_data) {
-  document.write('<ul class="recent_posts_arlina">');
-  var j = 0;
-  for (; j < numposts; j++) {
-    var l;
-    var data = _data.feed.entry[j];
-    var video1title = data.title.$t;
-    if (j == _data.feed.entry.length) {
-      break;
-    }
-    var i = 0;
-    for (; i < data.link.length; i++) {
-      if ("replies" == data.link[i].rel && "text/html" == data.link[i].type) {
-        var spacingPx = data.link[i].title;
-        var entityUrl = data.link[i].href;
-      }
-      if ("alternate" == data.link[i].rel) {
-        l = data.link[i].href;
-        break;
-      }
-    }
-    var url_salt;
-    try {
-      url_salt = data.media$thumbnail.url;
-    } catch (h) {
-      s = data.content.$t;
-      a = s.indexOf("<img");
-      b = s.indexOf('src="', a);
-      c = s.indexOf('"', b + 5);
-      d = s.substr(b + 5, c - b - 5);
-      url_salt = -1 != a && -1 != b && -1 != c && "" != d ? d : "https://2.bp.blogspot.com/-Q6S8qhkE33I/V0VwhvhULoI/AAAAAAAAHfQ/VZkkOgl_wX4X59EP31Jpl1swFsj6-n0TQCLcB/s1600/InfoArlina.png";
-    }
-    var summary = data.published.$t;
-    var spacingRulerPx = summary.substring(0, 4);
-    var total_pageviews_raw = summary.substring(5, 7);
-    var _transactionName = summary.substring(8, 10);
-    var month = new Array;
-    if (month[1] = "Jan", month[2] = "Feb", month[3] = "Mar", month[4] = "Apr", month[5] = "May", month[6] = "Jun", month[7] = "Jul", month[8] = "Aug", month[9] = "Sep", month[10] = "Oct", month[11] = "Nov", month[12] = "Dec", document.write('<li class="clearfix">'), 1 == showpostthumbnails && document.write('<span class="wrapinfo"><img class="recent_thumb" src="' + url_salt + '"/></span>'), document.write('<b><a href="' + l + '" target ="_top">' + video1title + "</a></b>"), "content" in data) {
-      var id = data.content.$t;
-    } else {
-      if ("summary" in data) {
-        id = data.summary.$t;
-      } else {
-        id = "";
-      }
-    }
-    var VARS_RE = /<\S[^>]*>/g;
-    if (id = id.replace(VARS_RE, ""), 1 == showpostsummary) {
-      if (id.length < numchars) {
-        document.write("<i>");
-        document.write(id);
-        document.write("</i>");
-      } else {
-        document.write("<i>");
-        id = id.substring(0, numchars);
-        var indexOfSlash = id.lastIndexOf(" ");
-        id = id.substring(0, indexOfSlash);
-        document.write(id + "...");
-        document.write("</i>");
-      }
-    }
-    var x = "";
-  
-    var $ = 0;
-    document.write;
-    if (1 == showpostdate) {
-      
-      x = x + month[parseInt(total_pageviews_raw, 10)] + "-" + _transactionName + " - " + spacingRulerPx;
-      
-      $ = 1;
-    }
-    if (1 == showcommentnum) {
-      if (1 == $) {
-        
-        x = x + " | ";
-      }
-      if ("1 Comments" == spacingPx) {
-        
-        spacingPx = "1 Comment";
-      }
-      if ("0 Comments" == spacingPx) {
-        
-        spacingPx = "No Comments";
-      }
-      
-      spacingPx = '<a href="' + entityUrl + '" target ="_top">' + spacingPx + "</a>";
-      
-      x = x + spacingPx;
-      
-      $ = 1;
-    }
-    if (1 == displaymore) {
-      if (1 == $) {
-        
-        x = x + " | ";
-      }
-      
-      x = x + '<a href="' + l + '" class="url" target ="_top">More -></a>';
-      
-      $ = 1;
-    }
-    document.write(x);
-    document.write("</li>");
-    if (1 == displayseparator && j != numposts - 1) {
-      document.write("<hr size=0.5>");
-    }
-  }
-  document.write("</ul>");
-}
-;
+<style scoped='' type='text/css'>
+ul.recent_posts_arlina li{display:inline-block;position:relative;width:100%;margin:0;padding:10px 0;transition:all .3s linear;}
+ul.recent_posts_arlina li:last-child {border:0;}
+.recent_posts_arlina .wrapinfo{display:inline-block;margin:0 10px 0 0;overflow:hidden;z-index:2;position:relative;width:90px;height:62px;float:left}
+ul.recent_posts_arlina li img.recent_thumb{width:100%;height:auto}
+.recent_posts_arlina {float:left;width:100%;margin:10px 10px 5px 0px;padding:0;font-size:.9rem;}
+ul.recent_posts_arlina li a{color:#2b427d;font-size:13px;font-weight:700;line-height:normal}
+ul.recent_posts_arlina li a:hover{color:#138be6}
+.recent_posts_arlina i{font-style:normal;color:#999;font-size:13px}
+</style>
+<script style='text/javascript' src='https://rawcdn.githack.com/williamrinaldo/project/1ca3fde1e09eb9b42fafb9010678f830948d37c6/recentpostarlina.js'></script>
+<script style='text/javascript'>
+var numposts=5,showpostthumbnails=!0,displaymore=!1,displayseparator=!1,showcommentnum=!1,showpostdate=!1,showpostsummary=!1,numchars=62;
+</script>
+<script src='/feeds/posts/default?orderby=published&alt=json-in-script&callback=recentpostarlina'></script>
